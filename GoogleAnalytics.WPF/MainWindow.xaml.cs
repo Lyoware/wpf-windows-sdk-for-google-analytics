2010414e9ab4dc7f92dfb23c0f510a4723f45c2a
Lyoware <ajms.developer@gmail.com>
2019-09-19 18:26:31 -0500

    Events for debugging purposes were added

--- GoogleAnalytics.WPF/MainWindow.xaml.cs
+++ GoogleAnalytics.WPF/MainWindow.xaml.cs
@@ -26,9 +26,31 @@ namespace GoogleAnalytics.WPF
         {
             App.Tracker.ScreenName = "Main";
             App.Tracker.Page = "MainTestingSDK";
+            App.Tracker.Title = "MainPage";
             InitializeComponent();
+            isRunningInDebugMode = IsDebugRequest.IsChecked.Value;
+            // wire up to event for debugging purposes:
+            AnalyticsManager.Current.HitSent += AnalyticsManager_HitSent;
+            AnalyticsManager.Current.HitMalformed += AnalyticsManager_HitMalformed;
+            AnalyticsManager.Current.HitFailed += AnalyticsManager_HitFailed;
+        }
+    
+
+        private void AnalyticsManager_HitFailed(object sender, HitFailedEventArgs e)
+        {
+            Log(e.Hit, $"**Hit Failed** {Environment.NewLine} {e.Error.Message}");
+
         }
 
+        private void AnalyticsManager_HitMalformed(object sender, HitMalformedEventArgs e)
+        {
+            Log(e.Hit, $"**Hit Malformed ** {Environment.NewLine} {e.HttpStatusCode.ToString()}");
+        }
+
+        private void AnalyticsManager_HitSent(object sender, HitSentEventArgs e)
+        {
+            Log(e.Hit, e.Response);
+        }
         private void ButtonEvent_Click(object sender, RoutedEventArgs e)
         {
             App.Tracker.Send(HitBuilder.CreateCustomEvent("EventoTesting", "userclick", "lab1", 10).Build());
@@ -67,6 +89,46 @@ namespace GoogleAnalytics.WPF
             Visibility visibility = isRunningInDebugMode ? Visibility.Visible : Visibility.Collapsed;
             RequestPanel.Visibility = visibility;
             ResponsePanel.Visibility = visibility;
+        }
+        void Log(Hit hit, string message)
+        {
+            if (isRunningInDebugMode)
+            {
+                if (AnalyticsManager.Current.DispatchPeriod > TimeSpan.Zero)
+                {
+                    message = "Using dispatcher-which batches activity. Here is the last hit sent\n" + message;
+                }
+
+                if (Dispatcher.CheckAccess())
+                {
+                    Results.Text = message;
+                    Request.Text = Parse(hit);
+                }
+                else
+                {
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        Results.Text = message;
+                        Request.Text = Parse(hit);
+                    });
+                }
+
+            }
+            //Output to console regardless 
+            System.Diagnostics.Debug.WriteLine(message);
+        }
+
+        string Parse(Hit hit)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (hit != null)
+            {
+                foreach (string param in hit.Data.Keys)
+                {
+                    builder.Append($"{param}:{hit.Data[param]}{Environment.NewLine}");
+                }
+            }
+            return builder.ToString();
         }
     }
 }
